e76fc6f396b7688926de493508be21c7beb982d9
Kai Kreuzer <kai.kreuzer@telekom.de>
2025-01-03 10:32:59 +0100

    Align build script with other repos

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,21 +3,83 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
+import com.vanniktech.maven.publish.SonatypeHost
+import java.lang.System.getenv
+import java.net.URI
+
 plugins {
-    kotlin("jvm") version "2.0.21"
+    id("org.springframework.boot") version "3.4.0" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.2"
+    id("io.spring.dependency-management") version "1.1.6"
+    id("org.cadixdev.licenser") version "0.6.1"
+
+    id("com.citi.helm") version "2.2.0"
+    id("com.citi.helm-publish") version "2.2.0"
+    id("net.researchgate.release") version "3.0.2"
+    id("com.vanniktech.maven.publish") version "0.30.0"
+    kotlin("jvm")
+    kotlin("kapt") version "2.0.21"
     kotlin("plugin.serialization") version "2.0.21" apply false
-    id("org.jlleitschuh.gradle.ktlint") version "12.1.1" apply false
     id("org.jetbrains.kotlinx.kover") version "0.8.3"
     id("org.jetbrains.dokka") version "1.9.20"
-    id("org.springframework.boot") version "3.3.5" apply false
-    id("io.spring.dependency-management") version "1.1.6" apply false
-    id("net.researchgate.release") version "3.0.2" apply false
-    id("com.vanniktech.maven.publish") version "0.30.0" apply false
-    id("com.citi.helm") version "2.2.0" apply false
-    id("com.citi.helm-publish") version "2.2.0" apply false
 }
 
-val springBootVersion by extra { "3.3.5" }
+val springBootVersion by extra { "3.4.0" }
+
+fun getProperty(propertyName: String) = getenv(propertyName) ?: project.findProperty(propertyName) as String
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.DEFAULT)
+    signAllPublications()
+
+    pom {
+        name = "LMOS Runtime"
+        description =
+            """The LMOS Runtime facilitates dynamic agent routing and conversation handling in a multi-tenant, multi-channel environment.
+            """.trimMargin()
+        url = "https://github.com/eclipse-lmos/lmos-runtime"
+        licenses {
+            license {
+                name = "Apache-2.0"
+                distribution = "repo"
+                url = "https://github.com/eclipse-lmos/lmos-runtime/blob/main/LICENSES/Apache-2.0.txt"
+            }
+        }
+        developers {
+            developer {
+                id = "telekom"
+                name = "Telekom Open Source"
+                email = "opensource@telekom.de"
+            }
+        }
+        scm {
+            url = "https://github.com/eclipse-lmos/lmos-runtime.git"
+        }
+    }
+
+    release {
+        buildTasks = listOf("releaseBuild")
+        ignoredSnapshotDependencies =
+            listOf()
+        newVersionCommitMessage = "New Snapshot-Version:"
+        preTagCommitMessage = "Release:"
+    }
+
+    tasks.register("releaseBuild") {
+        dependsOn(subprojects.mapNotNull { it.tasks.findByName("build") })
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = URI("https://maven.pkg.github.com/eclipse-lmos/lmos-runtime")
+            credentials {
+                username = findProperty("GITHUB_USER")?.toString() ?: getenv("GITHUB_USER")
+                password = findProperty("GITHUB_TOKEN")?.toString() ?: getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
 
 repositories {
     mavenCentral()
@@ -31,6 +93,7 @@ subprojects {
     apply(plugin = "kotlinx-serialization")
     apply(plugin = "org.jetbrains.kotlinx.kover")
     apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    apply(plugin = "com.vanniktech.maven.publish")
 
     repositories {
         mavenCentral()
@@ -55,8 +118,6 @@ subprojects {
         useJUnitPlatform()
     }
 }
-
-fun getProperty(propertyName: String) = System.getenv(propertyName) ?: project.findProperty(propertyName) as String
 
 dependencies {
     kover(project("lmos-runtime-core"))
